24bb7cfa90fe7c974477f5fe1c56336c9d814977
md.kawsar <kawsar35-201@diu.edu.bd>
2023-05-16 09:47:27 +0600

    Implement multiple interface in a class at a time

--- src/main/java/com/bracit/loancollection/validation/LoanCollectionRequestValidator.java
+++ src/main/java/com/bracit/loancollection/validation/LoanCollectionRequestValidator.java
@@ -6,11 +6,16 @@ import org.springframework.stereotype.Component;
 
 @Component
 @RequiredArgsConstructor
-public class LoanCollectionRequestValidator implements LoanValidationForCollection {
+public class LoanCollectionRequestValidator implements LoanValidationForCollection, MemberLoanCollectionValidation, PaymentSubTypeValidationForCollection {
 
 
     @Override
     public boolean isLienLoan(Long memberId) {
+        return false;
+    }
+
+    @Override
+    public boolean paymentSubTyeCheck(int id) {
         return false;
     }
 }
